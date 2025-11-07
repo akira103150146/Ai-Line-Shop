9d1f4056315d798c19efea628f3b899b9325ab18
Akira <sunroad0418@gmail.com>
2025-11-07 15:43:46 +0800

    remove frontend folder

--- backend/ai_service/settings.py
+++ backend/ai_service/settings.py
@@ -110,7 +110,7 @@ ROOT_URLCONF = 'ai_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR.parent / 'frontend'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
