8637a725a540b7e0391bd63e9b38e5faec551a72
Akira <sunroad0418@gmail.com>
2025-11-26 12:01:15 +0800

    tailwind css cdn -> tailwind css static

---
+++ backend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./templates/**/*.html",
+        "./lut2/templates/**/*.html",
+        "./line_bot/templates/**/*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
